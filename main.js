99438f74503df985e847b735952b7e7206be01cb
Bang@Apr20 <santhoshrvcs@gmail.com>
2020-05-13 13:44:15 +0000

    Lab 4

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ class IpAddress {
     // For more information, consult the Log Class guide on the Itential
     // Developer Hub https://developer.itential.io/ located
     // under Documentation -> Developer Guides -> Log Class Guide
-    log.info('Starting the IpAddress product.');
+    //log.info('Starting the IpAddress product.');
 
 
   }
