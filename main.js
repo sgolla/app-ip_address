53837fb6acb1b66e23d9e0824e682bca62b3a467
Bang@Apr20 <santhoshrvcs@gmail.com>
2020-05-13 14:09:43 +0000

    Lab 4

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
   let firstIpAddress = null;
+  let ipv6Address = null;
   let callbackError = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
